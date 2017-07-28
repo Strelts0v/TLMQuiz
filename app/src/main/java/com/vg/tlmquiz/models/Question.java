dd6afa77aa5e46c4f51c32aa2b3ca7c98a952bbc
gleb.streltsov <gleb.streltsov.4by@gmail.com>
2017-07-28 15:44:32 +0300

    add Question class

---
+++ app/src/main/java/com/vg/tlmquiz/models/Question.java
@@ -0,0 +1,34 @@
+package com.vg.tlmquiz.models;
+
+/**
+ * Model for representing quiz question
+ */
+public class Question {
+
+    /** Id from strings.xml with text of the question */
+    private int mTextResId;
+
+    /** Property with answer on question */
+    private boolean mAnswerTrue;
+
+    public Question(int textResId, boolean answerTrue) {
+        mTextResId = textResId;
+        mAnswerTrue = answerTrue;
+    }
+
+    public int getTextResId() {
+        return mTextResId;
+    }
+
+    public void setTextResId(int textResId) {
+        mTextResId = textResId;
+    }
+
+    public boolean isAnswerTrue() {
+        return mAnswerTrue;
+    }
+
+    public void setAnswerTrue(boolean answerTrue) {
+        mAnswerTrue = answerTrue;
+    }
+}
